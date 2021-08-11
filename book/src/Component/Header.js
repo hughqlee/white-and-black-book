63fcf49fec600c4b61384de8de3487b7d696423e
이현규 <hughqlee@ihyeongyuui-MacBookAir.local>
2021-08-11 16:08:05 +0900

    tutorial

---
+++ book/src/Component/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+      <header>
+        <h1>White & Black Book</h1>
+        <nav>
+          <li>PaPa</li>
+          <li>MaMa</li>
+          <li>TRONG</li>
+        </nav>
+      </header>
+    )
+  }
+  
+  export default Header;
+  
